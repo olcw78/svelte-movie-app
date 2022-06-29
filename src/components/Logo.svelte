ef81eb06c74ab4d2a9b67eea22bf91b508c93674
yoonsang lee <highp0912@pm.me>
2022-06-29 17:24:45 +0900

    add: Logo

---
+++ src/components/Logo.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { link } from 'svelte-spa-router'
+</script>
+
+<a use:link href="/"> <span>OMDb API</span>.COM</a>
+
+<style lang="scss">
+  a {
+    display: block;
+    font-family: 'oswald', sans-serif;
+    font-size: 20px;
+    color: rgba(#fff, 0.5);
+    text-decoration: none;
+
+    span {
+      color: #fdc000;
+    }
+  }
+</style>
