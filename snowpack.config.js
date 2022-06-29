2ef1d41dd9feb2b1c29e3edcc14224f11d0dfa11
yoonsang lee <highp0912@pm.me>
2022-06-29 14:39:17 +0900

    snowpack 설정

---
+++ snowpack.config.js
@@ -0,0 +1,39 @@
+const isProduction =
+  process.env.NODE_ENV?.toLowerCase() === 'production'
+
+function babelOptions() {
+  return {
+    plugins: isProduction ? ['transform-remove-console'] : []
+  }
+}
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/dist'
+  },
+  alias: {
+    '@': './src'
+  },
+  plugins: [
+    [
+      '@snowpack/plugin-svelte',
+      {
+        preprocess: require('svelte-preprocess')({
+          scss: {
+            prependData: '@import "./src/style/main.scss";'
+          },
+          postcss: { plugins: [require('autoprefixer')()] },
+          babel: babelOptions()
+        })
+      }
+    ],
+    [
+      '@snowpack/plugin-babel',
+      { transformOptions: babelOptions() }
+    ],
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-optimize'
+  ]
+}
---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Header from '@/components/Header.svelte'
+</script>
+
+<Header />
+<h1>Svelte init!</h1>
+
+<style lang="scss">
+</style>
